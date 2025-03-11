587fd8623cd073fe3df0c22170b30090b952e2ff
goesbams <goesbamsday@gmail.com>
2025-03-11 23:15:37 +0700

    feat: add unit test generate short code helper

--- internal/helpers/helpers_test.go
+++ internal/helpers/helpers_test.go
@@ -1,1 +1,17 @@
 package helpers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateShortCode(t *testing.T) {
+	originalURL := "https://test.test"
+	length := 6
+
+	shortCode := GenerateShortCode(originalURL, length)
+	assert.Equal(t, length, len(shortCode))
+	assert.Equal(t, GenerateShortCode(originalURL, length), shortCode, "Short code generation should be deterministic")
+	assert.NotEqual(t, length, GenerateShortCode(originalURL, 10), "Short code length should not be equal to the length parameter")
+}

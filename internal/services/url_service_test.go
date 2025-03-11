638cd400cbb6c4741807aa2ef6edd959eed04df5
goesbams <goesbamsday@gmail.com>
2025-03-11 22:51:11 +0700

    feat: add unit test shorten url service

--- internal/services/url_service_test.go
+++ internal/services/url_service_test.go
@@ -1,1 +1,83 @@
 package services
+
+import (
+	"context"
+	"errors"
+	"mini-url-shortener/internal/models"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockURLRepository struct {
+	mock.Mock
+}
+
+func (r *MockURLRepository) Begin(ctx context.Context) error {
+	args := r.Called(ctx)
+	return args.Error(0)
+}
+
+func (r *MockURLRepository) Commit() error {
+	args := r.Called()
+	return args.Error(0)
+}
+
+func (r *MockURLRepository) Rollback() error {
+	args := r.Called()
+	return args.Error(0)
+}
+
+func (m *MockURLRepository) CreateShortCode(ctx context.Context, url *models.URL) error {
+	args := m.Called(ctx, url)
+	return args.Error(0)
+}
+
+func TestShortenURL_Success(t *testing.T) {
+	mockRepo := new(MockURLRepository)
+	service := NewURLService(mockRepo)
+
+	url := &models.URL{
+		OriginalURL: "https://test.test",
+		ShortCode:   "a3f6fe",
+	}
+
+	mockRepo.On("CreateShortCode", context.Background(), url).Return(nil)
+
+	shortCode, err := service.ShortenURL(context.Background(), url.OriginalURL)
+	assert.NoError(t, err)
+
+	assert.Equal(t, url.ShortCode, shortCode)
+	assert.Equal(t, "a3f6fe", shortCode)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestShortenURL_EmptyURL(t *testing.T) {
+	mockRepo := new(MockURLRepository)
+	service := NewURLService(mockRepo)
+
+	_, err := service.ShortenURL(context.Background(), "")
+	assert.Error(t, err)
+	assert.Equal(t, "original url cannot be empty", err.Error())
+	mockRepo.AssertExpectations(t)
+}
+
+func TestShortenURL_RepositoryError(t *testing.T) {
+	mockRepo := new(MockURLRepository)
+	service := NewURLService(mockRepo)
+
+	url := &models.URL{
+		OriginalURL: "https://test.test",
+		ShortCode:   "a3f6fe",
+	}
+
+	mockRepo.On("CreateShortCode", context.Background(), url).Return(errors.New("database error"))
+
+	shortCode, err := service.ShortenURL(context.Background(), url.OriginalURL)
+	assert.Error(t, err)
+
+	assert.Empty(t, shortCode)
+	assert.Equal(t, "error creating shorten url: database error", err.Error())
+	mockRepo.AssertExpectations(t)
+}
